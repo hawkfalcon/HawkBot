768890f6abc438f51feda516a7016a32dbcbecc9
Nick <3playgames99@gmail.com>
2014-06-11 13:50:15 -0700

    OMG UPDATE

---
+++ src/main/java/com/hawkfalcon/bot/Bot.java
@@ -0,0 +1,14 @@
+package com.hawkfalcon.bot;
+
+import org.pircbotx.PircBotX;
+
+/**
+ * Created by Nick Parks on 6/11/2014 at 1:27 PM.
+ */
+public class Bot extends PircBotX{
+
+    public Bot(){
+        this.setName("Hawkbotx2");
+    }
+
+}
